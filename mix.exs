bdd7268811fe9b25036a8ba3ad9b76a6c9858bbf
m.malkin <mmalkin@fun-box.ru>
2021-04-22 19:24:27 +0300

    Version bumpup: 0.0.1 → 0.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pcapex.MixProject do
   def project do
     [
       app: :pcapex,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
