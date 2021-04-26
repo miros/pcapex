9ad993993e7c097687efcb9f388484c43ad7ba57
m.malkin <mmalkin@fun-box.ru>
2021-04-26 15:25:30 +0300

    cover.lint task added to ci; excoveralls upload removed

--- mix.exs
+++ mix.exs
@@ -11,21 +11,22 @@ defmodule Pcapex.MixProject do
       package: package(),
       dialyzer: dialyzer(),
       description: "Simple library in pure Elixir for encoding and decoding pcap file data",
-      test_coverage: [tool: ExCoveralls]
+      test_coverage: [tool: ExCoveralls],
+      aliases: aliases(),
+      preferred_cli_env: [
+        cover: :test,
+        "cover.detail": :test,
+        "cover.html": :test,
+        "cover.filter": :test,
+        "cover.lint": :test,
+        dialyzer: :test
+      ]
     ]
   end
 
   def application do
     [
       extra_applications: [:logger]
-    ]
-  end
-
-  defp deps do
-    [
-      {:excoveralls, "~> 0.13", only: :test},
-      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev, :test], runtime: false},
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
@@ -44,6 +45,27 @@ defmodule Pcapex.MixProject do
   def dialyzer() do
     [
       flags: [:unmatched_returns, :error_handling, :race_conditions, :unknown]
+    ]
+  end
+
+  defp aliases do
+    [
+      test: ["test --no-start"],
+      cover: ["coveralls --sort cov:desc"],
+      "cover.lint": [
+        "coveralls.lint --required-project-coverage=0.9 --missed-lines-threshold=2 --required-file-coverage=0.9"
+      ],
+      "cover.html": ["coveralls.html"],
+      "cover.detail": ["coveralls.detail --filter"]
+    ]
+  end
+
+  defp deps do
+    [
+      {:excoveralls, "~> 0.13", only: :test},
+      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev, :test], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:excoveralls_linter, "~> 0.2.0", only: :test}
     ]
   end
 end
